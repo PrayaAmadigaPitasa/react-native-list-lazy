e1f912459683503ab0e22c8bbeba6e298f7a8e21
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-02-03 18:31:09 +0700

    chore: remove default object partial

--- src/types/object/ObjectType.ts
+++ src/types/object/ObjectType.ts
@@ -9,5 +9,4 @@ import {
 export type ObjectRef<T> = RefObject<T> | null | ((instance: T | null) => void);
 export type ObjectKey = string | number;
 export type ObjectState<S> = [S, Dispatch<SetStateAction<S>>];
-export type ObjectPartial<P> = {[K in keyof P]?: P[K]};
 export type ObjectElement = ComponentType<any> | ReactElement | null;
